0f9b2647ee10ac0ac634fd15b4b950adb4e867e7
YTfebr22 <mazegferbahij@gmail.com>
2026-08-14 22:01:15 +0500

    rewritten using OOP

---
+++ engine/api/Api.h
@@ -0,0 +1,43 @@
+#ifndef NAKEDENGINE_WINDOWENGINE_H
+#define NAKEDENGINE_WINDOWENGINE_H
+#include <iostream>
+#include <string>
+#include <GLFW/glfw3.h>
+
+
+class Window {
+private:
+    const std::string TITLE = "GLFW Window";
+    const int32_t WIDTH = 640;
+    const int32_t HEIGHT = 480;
+    const bool FULLSCREEN = false;
+
+    GLFWwindow* mwindow;
+public:
+    Window() {
+        mwindow = glfwCreateWindow(WIDTH, HEIGHT, TITLE.c_str(), (!FULLSCREEN) ? nullptr : glfwGetPrimaryMonitor(), nullptr);
+        if (WIDTH <= 0 || HEIGHT <= 0) {
+            throw std::runtime_error("GLFW Error: Height and width must be greater than 0");
+        } else if (!mwindow) {
+                throw std::runtime_error("Failed to create GLFW Window");
+        }
+    };
+
+    ~Window() {
+        glfwTerminate();
+    }
+
+    void ContextCurrent() {
+        glfwMakeContextCurrent(mwindow);
+    }
+
+    void loop() {
+        while (!glfwWindowShouldClose(mwindow)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(mwindow);
+            glfwPollEvents();
+        }
+    }
+};
+
+#endif //NAKEDENGINE_WINDOWENGINE_H
--- engine/main.cpp
+++ engine/main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-#include <GLFW/glfw3.h>
+// #include <glad/glad.h>
+#include "api/Api.h"
 
-int main(void) {
-    GLFWwindow* window;
+const uint32_t WIDTH = 640;
+const uint32_t HEIGHT = 480;
+const std::string TITLE = "GLFW Window";
+const bool FULLSCREEN = false;
 
-    glfwInit();
+int main() {
+    try {
+        glfwInit();
 
-    window = glfwCreateWindow(640,480, "Hello World", nullptr, nullptr);
-    if (!window) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
+        Window engine;
+
+        engine.ContextCurrent();
+        engine.loop();
+        glViewport(0, 0, WIDTH, HEIGHT);
+
+        return 0;
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
     }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwTerminate();
-    return 0;
 }
